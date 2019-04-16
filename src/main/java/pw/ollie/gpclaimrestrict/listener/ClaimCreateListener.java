9b02bdcd462a22a920f9718ef886ac47cf7a3e7f
Oliver <o.dzineit@gmail.com>
2019-04-16 11:41:59 +0100

    Add initial version of proximity check

--- src/main/java/pw/ollie/gpclaimrestrict/listener/ClaimCreateListener.java
+++ src/main/java/pw/ollie/gpclaimrestrict/listener/ClaimCreateListener.java
@@ -1,6 +1,7 @@
 package pw.ollie.gpclaimrestrict.listener;
 
 import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import me.ryanhamshire.GriefPrevention.events.ClaimCreatedEvent;
 
 import pw.ollie.gpclaimrestrict.GPCRPlugin;
@@ -8,11 +9,15 @@ import pw.ollie.gpclaimrestrict.util.Cuboid;
 import pw.ollie.gpclaimrestrict.util.MathUtil;
 import pw.ollie.gpclaimrestrict.util.Vector3i;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+
+import java.util.Collection;
+import java.util.Objects;
 
 public final class ClaimCreateListener implements Listener {
     private final GPCRPlugin plugin;
@@ -31,8 +36,6 @@ public final class ClaimCreateListener implements Listener {
             return;
         }
 
-        // todo add nearby trusted list
-
         Claim claim = event.getClaim();
         int restrictedRadius = plugin.getConfiguration().getNearbyRestrictionRadius();
         Vector3i restrictionSize = new Vector3i(restrictedRadius, restrictedRadius, restrictedRadius);
@@ -46,6 +49,33 @@ public final class ClaimCreateListener implements Listener {
         Cuboid restrictionArea = new Cuboid(restrictedMin.toLocation(claimMin.getWorld()), restrictedMax.subtract(restrictedMin));
 
         // now we must check whether any existing claims fall into this area - in which case we cancel the event
-        // todo
+        Collection<Claim> claims = GriefPrevention.instance.dataStore.getClaims();
+
+        for (Claim existingClaim : claims) {
+            Location vertex1 = existingClaim.getGreaterBoundaryCorner();
+            if (!Objects.equals(vertex1.getWorld(), claimMin.getWorld())) {
+                continue;
+            }
+
+            Location vertex2 = existingClaim.getLesserBoundaryCorner();
+
+            int xSize = vertex1.getBlockX() - vertex2.getBlockX();
+            int ySize = vertex1.getBlockY() - vertex2.getBlockY();
+            int zSize = vertex1.getBlockZ() - vertex2.getBlockZ();
+
+            Location vertex3 = vertex1.add(xSize, ySize, 0);
+            Location vertex4 = vertex1.add(xSize, 0, zSize);
+            Location vertex5 = vertex1.add(0, ySize, zSize);
+            Location vertex6 = vertex1.add(0, 0, zSize);
+            Location vertex7 = vertex1.add(0, ySize, 0);
+            Location vertex8 = vertex1.add(xSize, 0, 0);
+
+            if (MathUtil.containsAny(restrictionArea, vertex1, vertex2, vertex3, vertex4, vertex5, vertex6, vertex7, vertex8)) {
+                // todo check if the existing claim owner trusts the claimer
+                creator.sendMessage(ChatColor.RED + "You are too close to somebody else's claim!");
+                event.setCancelled(true);
+                return;
+            }
+        }
     }
 }
--- src/main/java/pw/ollie/gpclaimrestrict/util/MathUtil.java
+++ src/main/java/pw/ollie/gpclaimrestrict/util/MathUtil.java
@@ -7,6 +7,16 @@ public final class MathUtil {
         return new Vector3i(one.getBlockX() + two.getX(), one.getBlockY() + two.getY(), one.getBlockZ() + two.getZ());
     }
 
+    public static boolean containsAny(Cuboid cuboid, Location... locations) {
+        for (Location location : locations) {
+            if (cuboid.contains(location)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private MathUtil() {
         throw new UnsupportedOperationException();
     }
